d06a8ad48a55dacb07b0c29707415b05ecfd9fc0
James Zhou <James Zhou>
2017-03-19 11:36:58 -0700

    added voice commands

--- FoodApp/app/src/main/java/com/example/james/foodapp/FoodActivity.java
+++ FoodApp/app/src/main/java/com/example/james/foodapp/FoodActivity.java
@@ -1,9 +1,11 @@
 package com.example.james.foodapp;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,6 +31,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -84,6 +87,7 @@ public class FoodActivity extends AppCompatActivity {
                 if(foodStr.isEmpty()) {
                     Toast msg = Toast.makeText(getBaseContext(), "Please enter something", Toast.LENGTH_LONG);
                     msg.show();
+                    promptSpeech();
                 }
                 else {
                     Toast msg = Toast.makeText(getBaseContext(), "Updating Daily Profile", Toast.LENGTH_LONG);
@@ -178,8 +182,37 @@ public class FoodActivity extends AppCompatActivity {
 
         }
     }
+    public void promptSpeech() {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
 
+        try {
+            startActivityForResult(i, 100);
+        } catch(ActivityNotFoundException a) {
+            Toast.makeText(FoodActivity.this, "Your device does not support speech language", Toast.LENGTH_SHORT).show();
 
+        }
+
+    }
+    public void onActivityResult(int request_code, int result_code, Intent i) {
+        super.onActivityResult(request_code, result_code, i);
+        switch(request_code) {
+            case 100:
+                if(result_code == RESULT_OK && i != null) {
+
+                    ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast msg = Toast.makeText(getBaseContext(), "Updating Daily Profile", Toast.LENGTH_LONG);
+                    msg.show();
+                    // send to fire base
+                    loadWeatherData(result.get(0));
+
+                }
+
+                break;
+        }
+    }
 
     //To show a list of bluetooth paired Devices
     private void loadList(String name)
